3379e790c6309e483d5906a8e801fa04bcaa9a4d
Anant13T <ananttayal13@gmail.com>
2022-07-03 18:28:39 +0530

    4

--- studentstudyportal/dashboard/forms.py
+++ studentstudyportal/dashboard/forms.py
@@ -1,3 +1,6 @@
+from dataclasses import field
+from datetime import datetime
+from tkinter import Widget
 from django import forms
 from . models import *
 
@@ -5,3 +8,12 @@ class NotesForm(forms.ModelForm):
     class Meta:
         model=Notes
         fields=["title","description"]
+
+class DateInput(forms.DateInput):
+    input_type='date'
+
+class HomeworkForm(forms.ModelForm):
+    class Meta:
+        model=Homework
+        Widgets={'due':DateInput()}
+        fields=["subject","title","description","due","is_finished"]
